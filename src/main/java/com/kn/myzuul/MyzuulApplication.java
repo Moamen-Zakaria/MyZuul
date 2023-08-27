45f5b81735f5c2e94c4f8dcbc16ec2525ee5b582
Moamen Zakaria <mzakaria@TradeNET.COM>
2023-08-27 10:25:35 +0300

    skeleton

---
+++ src/main/java/com/kn/myzuul/MyzuulApplication.java
@@ -0,0 +1,13 @@
+package com.kn.myzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyzuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyzuulApplication.class, args);
+	}
+
+}
